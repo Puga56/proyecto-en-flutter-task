ac7d77cda3f65d2bacdebead9619f4edb8ab56ce
manuel88999 <manuelecheverria948@gmail.com>
2026-02-19 21:09:59 -0600

    Se hizo una modificacion en el appRouter - Manuel Echeverria

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,18 +1,16 @@
 import 'package:go_router/go_router.dart';
-// Importa tus pantallas aquí
+// importamos las pantallas
 import '../../presentation/screens/home_screen.dart';
 import '../../presentation/screens/create_task_screen.dart';
 import '../../presentation/screens/task_detail_screen.dart';
 
+// Configuración de las rutas para navegar
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    GoRoute(path: '/', builder: (context, state) => HomeScreen()),
     GoRoute(
-        path: '/create-task',
-        builder: (context, state) => const CreateTaskScreen()),
-    GoRoute(
-        path: '/details',
-        builder: (context, state) => const TaskDetailScreen()),
+        path: '/create-task', builder: (context, state) => CreateTaskScreen()),
+    GoRoute(path: '/details', builder: (context, state) => TaskDetailScreen()),
   ],
 );
