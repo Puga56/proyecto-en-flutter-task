64f46226b1dace27633606a28b056ed143d67b10
Puga56 <dzibpuga87@gmail.com>
2026-02-23 19:35:43 -0600

    Docs: Actualización de README y mejoras de código

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,13 +4,18 @@ import '../../presentation/screens/home_screen.dart';
 import '../../presentation/screens/create_task_screen.dart';
 import '../../presentation/screens/task_detail_screen.dart';
 
-// Configuración de las rutas para navegar
+// Esta es la configuración básica del router para movernos por la app
 final appRouter = GoRouter(
+// Que empiece siempre en el home al abrir la app
   initialLocation: '/',
   routes: [
+// La pantalla principal
     GoRoute(path: '/', builder: (context, state) => HomeScreen()),
     GoRoute(
-        path: '/create-task', builder: (context, state) => CreateTaskScreen()),
+        // Ruta para cuando queramos crear una tarea nueva
+        path: '/create-task',
+        builder: (context, state) => CreateTaskScreen()),
+    // Ruta para ver los detalles de la tarea (luego igual hay que pasarle el ID)
     GoRoute(path: '/details', builder: (context, state) => TaskDetailScreen()),
   ],
 );
